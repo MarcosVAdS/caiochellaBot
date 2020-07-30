1227f712773268e10faa48d1a65975fd94cfa55f
Marcos Almeida <35110825+MarcosVAdS@users.noreply.github.com>
2020-07-29 21:22:31 -0300

    clean cod and remove unusable code

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import redis
 from telegram.ext import Updater, CommandHandler, Handler
 import datetime 
 from conf.settings import TELEGRAM_TOKEN
-#import pandas as pd
+
 
 class Bot:
     def __init__(self):
@@ -19,30 +19,16 @@ class Bot:
 
     def start(self, bot, update):
         me = bot.get_me()
-        #main_menu_keyboard = [[telegram.KeyboardButton('/quanto')]]
-
-        #reply_kb_markup = telegram.ReplyKeyboardMarkup(main_menu_keyboard,
-        #                                            resize_keyboard=True,
-        #                                            one_time_keyboard=True)
-
         bot.send_message(chat_id=update.message.chat_id,
                         text=self.msg,
                         reply_markup=reply_kb_markup)
 
     def calculateDate(self):
-        print(self.d1)
-        print(self.d2)
-        #print(abs(self.d2 - self.d1).days)
         return abs((self.d2 - self.d1).days)
     
     def data(self, bot, update):
         data = str(self.calculateDate())
         bot.send_message(chat_id=update.message.chat_id, text= 'faltam só ' + data + ' dias' + ' pro rock doido 😎!')
-
-
-#db = redis.StrictRedis(host=config['DB']['host'],
-#                       port=config['DB']['port'],
-#                       db=config['DB']['db'])
 
 bot = Bot()
 updater = Updater(token=TELEGRAM_TOKEN)
